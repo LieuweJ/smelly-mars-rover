84a3606b2059dc464b473c89a48cab4fc64e2337
Lieuwe Jansons <lieuwe.jansons@ns.nl>
2025-10-10 08:53:13 +0200

    R

--- typescript/tests/Rover.test.ts
+++ typescript/tests/Rover.test.ts
@@ -1,29 +1,29 @@
-import { Rover } from '../src/Rover';
+import {Rover} from '../src/Rover';
 
 describe("MarsRoverShould", () => {
     test.each([
-      ["1 2 N", "", "1 2 N"],
-      ["1 2 N", "L", "1 2 W"],
-      ["1 2 W", "L", "1 2 S"],
-      ["1 2 S", "L", "1 2 E"],
-      ["1 2 E", "L", "1 2 N"],
-      ["1 2 N", "R", "1 2 E"],
-      ["1 2 E", "R", "1 2 S"],
-      ["1 2 S", "R", "1 2 W"],
-      ["1 2 W", "R", "1 2 N"],
-      ["1 2 N", "M", "1 3 N"],
-      ["1 2 E", "M", "2 2 E"],
-      ["1 2 S", "M", "1 1 S"],
-      ["1 2 W", "M", "0 2 W"],
-      ["1 2 N", "LMLMLMLMM", "1 3 N"],
-      ["3 3 E", "MMRMMRMRRM", "5 1 E"]
+        ["1 2 N", "", "1 2 N"],
+        ["1 2 N", "L", "1 2 W"],
+        ["1 2 W", "L", "1 2 S"],
+        ["1 2 S", "L", "1 2 E"],
+        ["1 2 E", "L", "1 2 N"],
+        ["1 2 N", "R", "1 2 E"],
+        ["1 2 E", "R", "1 2 S"],
+        ["1 2 S", "R", "1 2 W"],
+        ["1 2 W", "R", "1 2 N"],
+        ["1 2 N", "M", "1 3 N"],
+        ["1 2 E", "M", "2 2 E"],
+        ["1 2 S", "M", "1 1 S"],
+        ["1 2 W", "M", "0 2 W"],
+        ["1 2 N", "LMLMLMLMM", "1 3 N"],
+        ["3 3 E", "MMRMMRMRRM", "5 1 E"]
     ])(
-      "start at '%s', with instructions '%s' => '%s'",
-      (startingPosition, instructions, expectedOutput) => {
-        const rover = new Rover(startingPosition);
-        rover.go(instructions);        
-        expect(rover.pos()).toBe(expectedOutput);
-      }
+        "start at '%s', with instructions '%s' => '%s'",
+        (startingPosition, instructions, expectedOutput) => {
+            const rover = new Rover(startingPosition);
+            rover.go(instructions);
+            expect(rover.pos()).toBe(expectedOutput);
+        }
     );
 
     test.each([
@@ -60,4 +60,4 @@ describe("MarsRoverShould", () => {
         const rover = new Rover("4 5 S");
         expect(rover.pos()).toBe("4 5 S");
     })
-  });
+});
